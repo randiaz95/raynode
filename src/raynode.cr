ed4584a27fbcabbf920d4ae8cb3b098afc6e468f
Randy Diaz <randydiaz@Randizo.local>
2019-12-14 00:28:55 -0500

    First Commit

---
+++ src/raynode.cr
@@ -0,0 +1,16 @@
+# TODO: Write documentation for `Raynode`
+module Raynode
+  VERSION = "0.1.0"
+  class Raynode
+    def initialize(routes, port)
+      server = HTTP::Server.new do |context|
+        context.response.content_type = "text/html"
+        context.response.print routes[context.request.path]
+      end
+
+      address = server.bind_tcp port
+      puts "Raynode Listening on http://#{address}"
+      server.listen
+    end
+  end
+end
